bfc2281decbbf10f5cdafa032f3ed0520f75ea93
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2024-06-04 22:17:48 +0700

    chore: Add blog header with about section and member list

---
+++ application/views/blog_header.php
@@ -0,0 +1,46 @@
+<header data-bs-theme="dark">
+  <div class="collapse text-bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="alert alert-secondary">About</h4>
+          <p class="text-body-secondary">BSI Mini Blog Project adalah aplikasi web sederhana yang memungkinkan pengguna untuk membuat, mengelola, dan berbagi postingan blog. Proyek ini dirancang sebagai platform blogging dasar yang menawarkan fitur-fitur inti seperti pembuatan, pengeditan, dan penghapusan artikel, serta tampilan daftar postingan.</p>
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="alert alert-secondary">Anggota</h4>
+          <ul class="list-unstyled">
+            <li><span>Tony Vetra Sugita</span></li>
+            <li><span>Harits Fadhlillah</span></li>
+            <li><span>Tegar Joko Kurniawan</span></li>
+            <li><span>Nugroho Adi Wibowo</span></li>
+            <li><span>Fatta Rahmanufal</span></li>
+            <li><a href="<?= base_url()?>admin/login" class="btn btn-info my-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+              <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+            </svg> Login Staff</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark fixed-top bg-dark shadow-sm">
+    <div class="container">
+      <a href="<?= base_url(); ?>" class="navbar-brand d-flex align-items-center">
+      <svg viewBox="0 0 1024 1024" class="icon" width="25" height="25" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M901.369 837.303c-0.47 7.123-0.377 8.108 0.276 2.958a92.259 92.259 0 0 1-2.984 11.333c2.209-4.456 1.778-3.673-1.295 2.352-7.122 13.739 5.552-4.566-4.637 6.951-8.399 9.496-4.862 5.493-16.07 11.301 4.554-2.047 3.603-1.942-2.852 0.321-15.152 4.513 7.519 0.516-8.695 1.038-7.121-0.47-8.107-0.377-2.958 0.279a91.511 91.511 0 0 1-11.331-2.985c4.453 2.211 3.671 1.778-2.35-1.294-13.741-7.124 4.563 5.552-6.954-4.637-9.493-8.399-5.489-4.865-11.299-16.072 2.048 4.556 1.94 3.604-0.319-2.851-4.516-15.154-0.519 7.521-1.042-8.694 0.468-7.123 0.378-8.109-0.277-2.959a92.427 92.427 0 0 1 2.985-11.332c-2.21 4.456-1.777 3.673 1.295-2.351 7.122-13.74-5.554 4.566 4.637-6.951 8.398-9.498 4.863-5.494 16.071-11.303-4.555 2.048-3.607 1.942 2.848-0.322 15.154-4.511-7.518-0.514 8.694-1.037 7.122 0.472 8.108 0.378 2.96-0.278a91.428 91.428 0 0 1 11.331 2.985c-4.451-2.211-3.669-1.779 2.353 1.295 13.74 7.122-4.563-5.553 6.953 4.635 9.493 8.401 5.49 4.866 11.299 16.075-2.046-4.558-1.94-3.607 0.32 2.847 4.514 15.154 0.516-7.521 1.041 8.696 2.345 72.488 115.059 72.718 112.706 0-2.683-82.919-66.045-146.282-148.963-148.962-82.867-2.681-146.407 70.103-148.96 148.962-2.681 82.866 70.101 146.408 148.96 148.961 82.868 2.682 146.411-70.101 148.963-148.961 2.353-72.72-110.361-72.489-112.706 0z" fill="#4A5699"></path><path d="M170.713 497.175a74.746 74.264 0 1 0 149.492 0 74.746 74.264 0 1 0-149.492 0Z" fill="#E5594F"></path><path d="M397.17 497.175a74.745 74.264 0 1 0 149.49 0 74.745 74.264 0 1 0-149.49 0Z" fill="#F0D043"></path><path d="M623.624 497.175a74.745 74.264 0 1 0 149.49 0 74.745 74.264 0 1 0-149.49 0Z" fill="#F39A2B"></path><path d="M817.646 508.811c-0.716 91.878-35.913 176.968-100.411 242.421-64.138 65.088-151.53 99.701-242.417 100.41-91.332 0.713-177.696-36.638-242.413-100.41-65.088-64.14-99.7-151.533-100.41-242.421-0.565-72.649-113.272-72.704-112.706 0C21.004 728.915 183.774 925.69 403.627 958.41 628.07 991.811 843.85 856.411 911.28 639.609c13.053-41.962 18.729-86.946 19.072-130.798 0.568-72.704-112.139-72.649-112.706 0z" fill="#4A5699"></path><path d="M131.997 487.045c0.715-91.875 35.91-176.967 100.41-242.42 64.138-65.088 151.527-99.705 242.413-100.415 91.334-0.711 177.7 36.64 242.417 100.415 65.087 64.142 99.702 151.534 100.411 242.42 0.566 72.646 113.273 72.705 112.707 0-1.717-220.106-164.493-416.879-384.346-449.602C321.566 4.038 105.791 139.449 38.363 356.249c-13.05 41.96-18.731 86.947-19.072 130.796-0.567 72.705 112.141 72.646 112.706 0z" fill="#C45FA0"></path></g></svg>
+        <strong class="px-2">BSI Mini Blog</strong>
+      </a>
+      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="<?php echo base_url()?>home/blog_kategori/Umum" class="nav-link px-2 text-white link-hover">Umum</a></li>
+          <li><a href="<?php echo base_url()?>home/blog_kategori/Pengumuman" class="nav-link px-2 text-white link-hover">Pengumuman</a></li>
+          <li><a href="<?php echo base_url()?>home/blog_kategori/Administrasi" class="nav-link px-2 text-white link-hover">Administrasi</a></li>
+        </ul>
+      <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action="<?= base_url().'home/search'?>" method="post">
+        <input type="search" class="form-control" placeholder="Cari..." aria-label="Search" id="keyword" name="keyword">
+      </form>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
